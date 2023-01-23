8d681b30496651d76e386e490fda2c9cc5bafcea
atifrani <axeltifrani@gmail.com>
2023-01-23 15:57:06 +0100

    add sql script

---
+++ employees.sql
@@ -0,0 +1,9 @@
+create table if not exists employee 
+(
+    id varchar(256),
+    fname varchar(256),
+    lname varchar(256),
+    gender varchar(256),
+    age integer,
+    location varchar(256)
+)
